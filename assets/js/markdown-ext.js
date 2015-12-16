7fc2ed63fe550242bdedb3dbab4e2707acffbd58
chris lee <chris.lee@daliworks.net>
2015-12-16 16:15:43 +0900

    external link is opened with new tab

--- assets/js/markdown-ext.js
+++ assets/js/markdown-ext.js
@@ -7,3 +7,18 @@ $(document).ready(function(){
       $(this).toggleClass('ui-closed ui-open');
       });
     });
+
+
+$.expr[':'].external = function(obj){
+          return !obj.href.match(/^mailto\:/)
+                  && (obj.hostname != location.hostname);
+      };
+
+      $(function(){
+        // Add 'external' CSS class to all external links
+        $('a:external').addClass('external');
+
+        // turn target into target=_blank for elements w external class
+        $(".external").attr('target','_blank');
+
+      })
